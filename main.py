b33e209a530d18c5476b5aa61530db5e9d80ba2d
danielgrigore256 <grigore.daniel.256@gmail.com>
2021-03-24 17:12:01 +0200

    update 3.3.1 Beta

--- main.py
+++ main.py
@@ -5,90 +5,108 @@ from Jokes import Jokes
 # function menu to generate 10 jokes
 def ten_joke_menu():
     while True:
-        ten_check = input("""What random jokes u want?
-                        (1) Random
-                        (2) By type
-                        """)
-        if ten_check == "1":
-            generate_ten_random()
-            break
-
-        if ten_check == "2":
-
-            while True:
-                ten_by_type_check = input("""What type you want ?
-                                    (1) Programming
-                                    (2) General""")
-                if ten_by_type_check == "1":
-                    generate_ten_by_type("programming")
-                    break
-
-                if ten_by_type_check == "2":
-                    generate_ten_by_type("general")
-                    break
-                else:
-                    print("Wrong number, let's try again")
-            break
+        try:
+            ten_check = int(input("""What random jokes u want?
+                            (1) Random
+                            (2) By type
+                            """))
+        except ValueError:
+            print("you didnt entered a number")
         else:
-            print("Wrong number, let's try again")
+            if ten_check == 1:
+                generate_ten_random()
+                break
+
+            elif ten_check == 2:
+
+                while True:
+                    try:
+                        ten_by_type_check = int(input("""What type you want ?
+                                                    (1) Programming
+                                                    (2) General"""))
+                    except ValueError:
+                        print("you didnt entered a number")
+                    else:
+                        if ten_by_type_check == 1:
+                            generate_ten_by_type("programming")
+                            break
+
+                        if ten_by_type_check == 2:
+                            generate_ten_by_type("general")
+                            break
+                        else:
+                            print("Not a valid choice, please try again")
+                break
+            else:
+                print("Not a valid choice, please try again")
 
 
 # function menu to generate 10 random jokes
 def generate_ten_random():
     ten_random_jokes = Jokes("ten")
     while True:
-        display_check = input("""How do you like to display them?
-                   (1) ALL 
-                   (2) Only ones with even ID
-                   (3) Only ones with odd ID """)
-
-        if display_check == "1":
-            for ten_check_index in range(len(ten_random_jokes.joke)):
-                ten_random_jokes.print_jokes(ten_check_index)
-            break
-        if display_check == "2":
-            ten_random_jokes.display_jokes_by_id_parity("even")
-            break
-        if display_check == "3":
-            ten_random_jokes.display_jokes_by_id_parity("odd")
-            break
+        try:
+            display_check = int(input("""How do you like to display them?
+                                    (1) ALL 
+                                    (2) Only ones with even ID
+                                    (3) Only ones with odd ID """))
+        except ValueError:
+            print("you didnt entered a number")
         else:
-            print("Wrong number, let's try again")
+            if display_check == 1:
+                for ten_check_index in range(len(ten_random_jokes.joke)):
+                    ten_random_jokes.print_jokes(ten_check_index)
+                break
+            if display_check == 2:
+                ten_random_jokes.display_jokes_by_id_parity("even")
+                break
+            if display_check == 3:
+                ten_random_jokes.display_jokes_by_id_parity("odd")
+                break
+            else:
+                print("Not a valid choice, please try again")
 
 
 # function to generate Ten random jokes by type
 def generate_ten_by_type(joke_type):
     ten_random_jokes_type = Jokes(f"{joke_type}/ten")
     while True:
-        type_check = input("""Do you want them ?
+        try:
+            type_check = int(input("""Do you want them ?
                                         (1)printed
-                                        (2) checked if same type""")
-        if type_check == "1":
-            for ten_check_index in range(len(ten_random_jokes_type.joke)):
-                ten_random_jokes_type.print_jokes(ten_check_index)
-            break
-        if type_check == "2":
-            ten_random_jokes_type.check_if_same_type(joke_type)
-            break
+                                        (2) checked if same type"""))
+        except ValueError:
+            print("you didnt entered a number")
         else:
-            print("Wrong number, let's try again")
+            if type_check == 1:
+                for ten_check_index in range(len(ten_random_jokes_type.joke)):
+                    ten_random_jokes_type.print_jokes(ten_check_index)
+                break
+            if type_check == 2:
+                ten_random_jokes_type.check_if_same_type(joke_type)
+                break
+            else:
+                print("Not a valid choice, please try again")
 
 
 # this is when Main input interface starts
 while True:
     try:
-        check = input(""" Hello, what joke do you want ? 
+        check = int(input(""" Hello, what joke do you want ? 
                         (1) Single joke
-                        (2) Ten Random Jokes""")
-        if check == "1":
+                        (2) Ten Random Jokes"""))
+    except ValueError:
+        print("you didnt entered a number")
+    else:
+        if check == 1:
             single_joke = Jokes("random")
             print("Here is your joke:")
             single_joke.print_jokes(0)
             break
-        elif check == "2":
+        elif check == 2:
             ten_joke_menu()
             break
         else:
-            raise ValueError
-    except:
-        print("try again")
+            print("Not a valid choice, please try again")
+
+
